b2c427b60f4cc904e49b070f7f79e92d0ae790db
JasperDre <jasperdre@gmail.com>
2018-01-26 23:29:30 +0100

    [ADDED] TopDownCamera

---
+++ Unreal/GlobalGameJam2018/Source/GlobalGameJam2018/TopDownCamera.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraActor.h"
+#include "TopDownCamera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLOBALGAMEJAM2018_API ATopDownCamera : public ACameraActor
+{
+	GENERATED_BODY()
+	
+public:
+	ATopDownCamera();
+
+	TArray<AActor> m_Players;
+};
