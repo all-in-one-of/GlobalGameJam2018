e753d390a25bc14603b9536e562143c8b9716f9e
Niels <paardekooperniels@gmail.com>
2018-01-26 20:28:11 +0100

    [ADD] Main Game Instance
    - Niels

---
+++ Unreal/GlobalGameJam2018/Source/GlobalGameJam2018/Instances/MainGameInstance.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MainGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLOBALGAMEJAM2018_API UMainGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Levels")
+	TArray<FName> LevelArray;
+
+	UFUNCTION(BlueprintCallable, Category = "Levels")
+	void ChangeCurrentLevel();
+	
+	
+};
