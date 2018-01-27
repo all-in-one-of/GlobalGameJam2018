dbcaf011765a923d3d4ac10fdf95dfe6c2603998
Timothy606 <Timjlucassen@gmail.com>
2018-01-27 05:43:28 +0100

    Cleaned up playerCharacter source file
    Cleaned up the code in the player character source file

--- Unreal/GlobalGameJam2018/Source/GlobalGameJam2018/playerCharacter.cpp
+++ Unreal/GlobalGameJam2018/Source/GlobalGameJam2018/playerCharacter.cpp
@@ -10,9 +10,7 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Kismet/GameplayStatics.h"
 #include "TopDownCamera.h"
-
-//////////////////////////////////////////////////////////////////////////
-// AthirdpersontemplateCharacter
+#include "Engine.h"
 
 APlayerCharacter::APlayerCharacter()
 {
@@ -34,23 +32,7 @@ APlayerCharacter::APlayerCharacter()
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
 
-	//// Create a camera boom (pulls in towards the player if there is a collision)
-	//CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	//CameraBoom->SetupAttachment(RootComponent);
-	//CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
-	//CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
-
-	//// Create a follow camera
-	//FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	//FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	//FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
-
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
 }
-
-//////////////////////////////////////////////////////////////////////////
-// Input
 
 void APlayerCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
 {
@@ -70,64 +52,19 @@ void APlayerCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerIn
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 	PlayerInputComponent->BindAxis("LookUpRate", this, &APlayerCharacter::LookUpAtRate);
 
-	// handle touch devices
-	PlayerInputComponent->BindTouch(IE_Pressed, this, &APlayerCharacter::TouchStarted);
-	PlayerInputComponent->BindTouch(IE_Released, this, &APlayerCharacter::TouchStopped);
-
-	// VR headset functionality
-	//PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AthirdpersontemplateCharacter::OnResetVR);
 }
 
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	TArray<AActor*> FoundCamera;
-	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ATopDownCamera::StaticClass(), FoundCamera);
-	if (FoundCamera.Num() == 1) 
-		MainCamera = Cast<ATopDownCamera>(FoundCamera[0]);
 
-	if (!MainCamera) 
-		UKismetSystemLibrary::QuitGame(GetWorld(), GetWorld()->GetFirstPlayerController(), EQuitPreference::Quit);
+	SetupCameraReference();
 
-	
-}
-
-
-//void AthirdpersontemplateCharacter::OnResetVR()
-//{
-//	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
-//}
-
-void APlayerCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
-{
-		Jump();
-}
-
-void APlayerCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
-{
-		StopJumping();
 }
 
 void APlayerCharacter::TurnAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
 	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
-	//const FRotator YawRotation(0, 0, 0);
-
-	//// get forward vector
-	//const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-	//AddMovementInput(Direction, 1.f);
-	//if ((Controller != NULL) && (Rate != 0.0f))
-	//{
-	//	// find out which way is forward
-	//	const FRotator Rotation = Controller->GetControlRotation();
-	//	const FRotator YawRotation(0, Rotation.Yaw + Rate, 0);
-
-	//	// get forward vector
-	//	//const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-	//	SetActorRotation(YawRotation);
-
-	//}
 }
 
 void APlayerCharacter::LookUpAtRate(float Rate)
@@ -162,4 +99,23 @@ void APlayerCharacter::MoveRight(float Value)
 		// add movement in that direction
 		AddMovementInput(Direction, Value);
 	}
+}
+
+void APlayerCharacter::SetupCameraReference() {
+
+	TArray<AActor*> FoundCamera;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ATopDownCamera::StaticClass(), FoundCamera);
+	if (FoundCamera.Num() == 1) {
+		MainCamera = Cast<ATopDownCamera>(FoundCamera[0]);
+	}
+	else {
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("[ERROR] Please place one TopDownCamera BP in the scene!"));
+		UKismetSystemLibrary::QuitGame(GetWorld(), GetWorld()->GetFirstPlayerController(), EQuitPreference::Quit);
+	}
+	if (!MainCamera)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("[ERROR] Failed to cast to TopDownCamera!"));
+		UKismetSystemLibrary::QuitGame(GetWorld(), GetWorld()->GetFirstPlayerController(), EQuitPreference::Quit);
+	}
+
 }
--- Unreal/GlobalGameJam2018/Source/GlobalGameJam2018/playerCharacter.h
+++ Unreal/GlobalGameJam2018/Source/GlobalGameJam2018/playerCharacter.h
@@ -31,47 +31,29 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
 	float BaseLookUpRate;
 
-protected:
-
-	/** Resets HMD orientation in VR. */
-	//void OnResetVR();
-
-	/** Called for forwards/backward input */
-	void MoveForward(float Value);
-
-	/** Called for side to side input */
-	void MoveRight(float Value);
-
-	/** 
-	 * Called via input to turn at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void TurnAtRate(float Rate);
-
-	/**
-	 * Called via input to turn look up/down at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void LookUpAtRate(float Rate);
-
-	/** Handler for when a touch input begins. */
-	void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
-
-	/** Handler for when a touch input stops. */
-	void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
-
-protected:
-	// APawn interface
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	// End of APawn interface
-
-	virtual void BeginPlay() override;
 	ATopDownCamera* MainCamera;
 
+protected:
+
+	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
+	void TurnAtRate(float Rate);
+
+	void LookUpAtRate(float Rate);
+
+protected:
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	virtual void BeginPlay() override;
+
 public:
-	///** Returns CameraBoom subobject **/
-	//FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	///** Returns FollowCamera subobject **/
-	//FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+	//Gets the camera reference and exits the game if no valid camera is setup
+	void SetupCameraReference();
+
+
 };
 
